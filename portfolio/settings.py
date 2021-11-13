e8c7674ee90ed1d077f4f5be039a66f8ec246a32
princeshobhit <shobhitraj34@gmail.com>
2021-11-13 18:51:33 +0530

    added local settings support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,3 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'Media_Files'
 
 MEDIA_URL = '/Media_Files/'
+
+try:
+    import .local_settings import *
+except ImportError:
+    pass
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,6 +22,6 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name = 'Home'),
+    path('', jobs.views.home, name='Home'),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
